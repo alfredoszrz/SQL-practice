01e8d3daf0b0a1c4f9b801ff7f91de9cb310aea8
alfredoszrz <alfredoszrz@gmail.com>
2020-01-29 23:03:02 -0600

    Avance en el ejercicio Grades

--- CodeSignal/Leaderboard.sql
+++ CodeSignal/Leaderboard.sql
@@ -5,3 +5,16 @@ BEGIN
     ORDER by score DESC
     LIMIT 3, 5;   
 END
+
+/*Grades*/
+
+CREATE PROCEDURE gradeDistribution()
+BEGIN
+	SELECT Name, Id 
+    FROM Grades
+    ORDER BY Name LIKE '___%' DESC
+    #CASE WHEN THEN
+    #CASE WHEN THEN
+    #ELSE
+    WHERE Final > Midterm1*0.25+Midterm2*0.25+Final*0.5 AND Final > Midterm1*0.5+Midterm2*0.5;
+END
